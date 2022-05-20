875aa1308ad399906224d5e59afbd0dbb8090f6f
Yuusuke <masngelox7@gmail.com>
2022-05-20 19:28:37 +0200

    Add twitch embed

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./index.scss";
+import {TwitchEmbed} from 'react-twitch-embed';
+import { useSelector } from "react-redux";
 
 export default function Index(){
     document.title = "Yuusuke - Index";
+    const darkMode = useSelector(state => state.darkMode).active;
     return(
-        <h1>
-        Index
-        </h1>
+        <div className="content">
+            <h1>Index</h1>
+            <div className="container">
+                <TwitchEmbed
+                    channel="Yuusuuke"
+                    theme="dark"
+                    width="100%"
+                    heigth="100%"
+                />
+            </div>
+        </div>
     );
 }
