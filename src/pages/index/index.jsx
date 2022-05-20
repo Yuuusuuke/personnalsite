efabd4f51cae5f5530483f47f5c5828421ce073b
Yuusuke <masngelox7@gmail.com>
2022-05-20 22:45:54 +0200

    Fill index page

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import "./index.scss";
 import {TwitchEmbed} from 'react-twitch-embed';
-import { useSelector } from "react-redux";
+import background from "../../ressources/indexBackground.jpg";
 
 export default function Index(){
     document.title = "Yuusuke - Index";
-    const darkMode = useSelector(state => state.darkMode).active;
     return(
-        <div className="content">
-            <h1>Index</h1>
-            <div className="container">
+        <div className="content backgroundIndex">
+            <img src={background} alt="" />
+            <div className="container container--index twitchEmbed">
                 <TwitchEmbed
                     channel="Yuusuuke"
                     theme="dark"
-                    width="100%"
-                    heigth="100%"
                 />
             </div>
         </div>
